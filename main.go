b75d9d7d41358dae986d4c6eaa0eb39ad4503ae6
nasa9084 <nasa9084@live.jp>
2018-04-21 15:06:34 +0900

    fix format

--- main.go
+++ main.go
@@ -141,15 +141,15 @@ func postToSlack(opts options, expiredCh, nearCh chan issue) error {
 		ec++
 		fmt.Fprintf(&buf, "- %s <%s|#%d>: %s(<%s>)\n", is.DueDate.Format("2006-01-02"), opts.Redmine.Endpoint, is.ID, is.Subject, getUser(opts, is.AssignedTo))
 	}
-	fmt.Fprintf(&out, "期限切れのチケットは%d件です\n", ec)
+	fmt.Fprintf(&out, "期限切れのチケットは*%d件*です\n", ec)
 	buf.WriteTo(&out)
 	buf.Reset()
 	var nc int
 	for is := range nearCh {
 		nc++
-		fmt.Fprintf(&buf, "- %s\n", is.DueDate.Format("2006-01-02"))
+		fmt.Fprintf(&buf, "- %s <%s|#%d>: %s(<%s>)\n", is.DueDate.Format("2006-01-02"), opts.Redmine.Endpoint, is.ID, is.Subject, getUser(opts, is.AssignedTo))
 	}
-	fmt.Fprintf(&out, "期限切れが近いチケットは%d件です\n", nc)
+	fmt.Fprintf(&out, "期限切れが近いチケットは*%d件*です\n", nc)
 	buf.WriteTo(&out)
 	if _, err := cli.Chat().PostMessage(opts.Slack.Channel).LinkNames(true).Text(out.String()).Do(context.Background()); err != nil {
 		return err
